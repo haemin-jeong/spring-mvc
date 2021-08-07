06ce56dc0a4cc8b36386fb64d646a5adc6d98e02
Haemin Jeong <jhm0828@gmail.com>
2021-08-07 18:38:37 +0900

    로그인 처리 - 회원 가입

--- login/src/main/java/hello/login/TestDataInit.java
+++ login/src/main/java/hello/login/TestDataInit.java
@@ -2,24 +2,33 @@ package hello.login;
 
 import hello.login.domain.item.Item;
 import hello.login.domain.item.ItemRepository;
+import hello.login.domain.member.Member;
+import hello.login.domain.member.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
 
+//편의상 테스트용 데이터 추가
 @Component
 @RequiredArgsConstructor
 public class TestDataInit {
 
     private final ItemRepository itemRepository;
+    private final MemberRepository memberRepository;
 
     /**
      * 테스트용 데이터 추가
      */
-    @PostConstruct
+    @PostConstruct //스프링 컨테이너 생성되고, 빈의 의존관계 주입이 완료된 시점에 호출
     public void init() {
         itemRepository.save(new Item("itemA", 10000, 10));
         itemRepository.save(new Item("itemB", 20000, 20));
-    }
 
+        Member member = new Member();
+        member.setLoginId("test");
+        member.setPassword("test!");
+        member.setName("테스터");
+        memberRepository.save(member);
+    }
 }
