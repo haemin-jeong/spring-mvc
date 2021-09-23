3d72a69334f189241714fc5b6f173cee9ecd0541
Haemin Jeong <jhm0828@gmail.com>
2021-09-24 01:04:22 +0900

    Validation - 오류 코드와 메시지 처리2
    rejectValue(), reject() 를 사용하여 검증 오류를 처리하도록 개선

--- validation/src/main/java/hello/itemservice/web/validation/ValidationItemControllerV2.java
+++ validation/src/main/java/hello/itemservice/web/validation/ValidationItemControllerV2.java
@@ -128,7 +128,7 @@ public class ValidationItemControllerV2 {
     }
 
     //메시지 코드를 사용하여 오류 메시지 출력
-    @PostMapping("/add")
+//    @PostMapping("/add")
     public String addItemV3(@ModelAttribute Item item, BindingResult bindingResult, RedirectAttributes redirectAttributes, Model model) {
 
         //검증 로직
@@ -154,6 +154,54 @@ public class ValidationItemControllerV2 {
             if (resultPrice < 10000) {
                 //글로벌 오류
                 bindingResult.addError(new ObjectError("item", new String[]{"totalPriceMin"}, new Object[]{10000, resultPrice}, null));
+            }
+        }
+
+        //검증에 실패하면 다시 입력 폼으로 이동
+        if (bindingResult.hasErrors()) {
+            log.info("errors = {}", bindingResult);
+            return "validation/v2/addForm";
+        }
+
+        //성공 로직
+        Item savedItem = itemRepository.save(item);
+        redirectAttributes.addAttribute("itemId", savedItem.getId());
+        redirectAttributes.addAttribute("status", true);
+        return "redirect:/validation/v2/items/{itemId}";
+    }
+
+    /*
+    rejectValue(), reject()를 사용하여 검증 오류처리
+    - rejectValue() : 필드 오류 처리
+    - reject() : 글로벌 오류 처리
+    - 두 메서드도 결국 내부적으로 FieldError, ObjectError를 사용해서 처리한다.
+     */
+    @PostMapping("/add")
+    public String addItemV4(@ModelAttribute Item item, BindingResult bindingResult, RedirectAttributes redirectAttributes, Model model) {
+
+        log.info("bindingResult.getObjectName() = {}", bindingResult.getObjectName()); //item
+        log.info("bindingResult.getTarget() = {}", bindingResult.getTarget()); //Item(id=null, itemName=, price=null, quantity=null)
+
+        //검증 로직
+        if (!StringUtils.hasText(item.getItemName())) {
+            //BindingResult는 어떤 객체를 대상으로 검증하는지 알고 있기 때문에 검증 객체 item에 대한 정보가 없어도 된다.
+            bindingResult.rejectValue("itemName", "required");
+        }
+
+        if (item.getPrice() == null || item.getPrice() < 1000 || item.getPrice() > 1000000) {
+            bindingResult.rejectValue("price", "range", new Object[]{1000, 1000000}, null);
+        }
+
+        if (item.getQuantity() == null || item.getQuantity() > 9999) {
+            bindingResult.rejectValue("quantity", "max", new Object[]{9999}, null);
+        }
+
+        //특정 필드가 아닌 복합 룰 검증
+        if (item.getPrice() != null && item.getQuantity() != null) {
+            int resultPrice = item.getPrice() * item.getQuantity();
+            if (resultPrice < 10000) {
+                //글로벌 오류
+                bindingResult.reject("totalPriceMin", new Object[]{10000, resultPrice}, null);
             }
         }
 
