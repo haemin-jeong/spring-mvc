278361989fb33e113542fa987cab0716bcf5b865
Haemin Jeong <jhm0828@gmail.com>
2021-09-27 17:56:06 +0900

    Bean Validation - 오브젝트 오류 처리
    @ScriptAssert를 사용하기보단 자바 코드를 작성하여 처리하자.

--- validation/src/main/java/hello/itemservice/domain/item/Item.java
+++ validation/src/main/java/hello/itemservice/domain/item/Item.java
@@ -2,12 +2,20 @@ package hello.itemservice.domain.item;
 
 import lombok.Data;
 import org.hibernate.validator.constraints.Range;
+import org.hibernate.validator.constraints.ScriptAssert;
 
 import javax.validation.constraints.Max;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 
 @Data
+/*
+@ScriptAssert로 오브젝트 오류 처리
+- 생성되는 메시지 코드 : ScriptAssert.item, ScriptAssert
+- 실제 사용시 제약이 많고 복잡. 실무에서는 객체의 범위를 넘어서는 검증 기능도 종종 필요한데 이 경우 대응이 어렵다.
+- 결론 : 오브젝트 오류는 자바 코드로 작성하는 것을 권장
+ */
+//@ScriptAssert(lang = "javascript", script = "_this.price * _this.quantity >= 10000")
 public class Item {
 
     private Long id;
